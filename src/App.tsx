cf7cbf20e9fb56642e8811676b8a1789fd287cff
Franco Traverso <franco.traverso@globallogic.com>
2022-08-25 21:05:10 -0300

    Add amount value through state bank

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux"
+import { bindActionCreators } from 'redux'
+import { actionCreators, State } from "./state";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(actionCreators, dispatch)
+  const amount = useSelector((state: State) => state.bank)
   return (
     <div className="App">
-      <h1>0</h1>
+      <h1>{amount}</h1>
       <button>Deposit</button>
       <button>Withdraw</button>
       <button>Bankrupt</button>
